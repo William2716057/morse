c8d2b4c22256ae9020983afa17d1dd60bb6258dd
User <Willspri@gmail.com>
2025-03-02 09:50:11 +1300

    updated

--- morse.ps1
+++ morse.ps1
@@ -2,10 +2,10 @@ $morseCode = @{
     'A' = '.-';    'B' = '-...';  'C' = '-.-.';  'D' = '-..';
     'E' = '.';     'F' = '..-.';  'G' = '--.';   'H' = '....';
     'I' = '..';    'J' = '.---';  'K' = '-.-';   'L' = '.-..';
-    'M' = '--';    'N' = '-.';    'O' = '---';   'P' = '.--.';
+    'M' = '--';    'N' = '-.';    'O' = '---';   'P' = '.--.'; 
     'Q' = '--.-';  'R' = '.-.';   'S' = '...';   'T' = '-';
     'U' = '..-';   'V' = '...-';  'W' = '.--';   'X' = '-..-';
-    'Y' = '-.--';  'Z' = '--..';  
+    'Y' = '-.--';  'Z' = '--..';
     '0' = '-----'; '1' = '.----'; '2' = '..---'; '3' = '...--';
     '4' = '....-'; '5' = '.....'; '6' = '-....'; '7' = '--...';
     '8' = '---..'; '9' = '----.'; 
@@ -14,7 +14,12 @@ $morseCode = @{
     ' ' = '/'
 }
 
-$message = Write-Host $morseCode['ss']
-foreach ($char in $string.ToCharArray()) {
-    Write-Host $message
+$message = 'SS'
+
+foreach ($char in $message.ToCharArray()) {
+    if ($morseCode.ContainsKey($char) 
+        Write-Host $morseCode[$char]
+    } else {
+        Write-Host "Character '$char' not found in Morse code table."
+    }
 }
